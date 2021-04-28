9b3cb3844a738bac859f7d8b2f2c56e6617b87da
Frosty <joeyfrost007@gmail.com>
2021-04-28 17:35:33 -0400

    Fixed messed up paranthesis in checklogin.php.

--- scripts/checkLogIn.php
+++ scripts/checkLogIn.php
@@ -1,8 +1,8 @@
 <?php
     require_once('constants.php');
-    
+
     // Check if user is not logged in, redirect to login page.
-    if (!(isset($_SESSION['user_id']) or $_SESSION['user_id'] == IS_DEFAULT)) {
+    if (!(isset($_SESSION['user_id'])) or ($_SESSION['user_id'] == IS_DEFAULT)) {
         header('Location: login.php');
     }
 ?>
