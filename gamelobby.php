c3fa7b0030428380460b27e834cf938da98eada6
Frosty <joeyfrost007@gmail.com>
2021-04-29 06:28:44 -0400

    Added determining whether or not the user is player 1 to the gamelobby.php.

--- gamelobby.php
+++ gamelobby.php
@@ -33,7 +33,52 @@
 
     // If the game is not started and they have a game id, then they 
     // should indeed be on this page. Proceed ;)
+
+
+
+    // Display Game Lobby Info 
+    // Also, determine which curent session player is player1 and 
+    // which is player2
     
+    $gameID = $_SESSION['game_id'];
+    $user_id = $_SESSION['user_id'];
+    $username = $_SESSION['username'];
+    $private = getIsPrivate($gameID); //from dbGetters.php
+    $players = getPlayers($gameID); //from dbGetters.php
+    $player1 = $players[0]; //player1 according to db (game creator)
+    $player2 = $players[1]; //player2 according to db
+                            //null if only 1 player in game
+    $user_is_player1 = false;
+
+    //--------------Session Info Prints --- DELETE LATER--------------
+    echo '<p>game_id: ';
+    echo $gameID;
+    echo "</p>";
+    echo '<p>user_id: ';
+    echo $user_id;
+    echo "</p>";
+    echo '<p>username: ';
+    echo $username; 
+    echo "</p>";
+    echo "<p>Game is private: "; 
+    echo $private;
+    echo "</p>";
+    echo "<p>Player1 ID: "; 
+    echo $player1;
+    echo "</p>";
+    echo "<p>Player2 ID: "; 
+    echo $player2;
+    echo "</p>";
+    //---------- End Session Info Prints -----------------------------
+
+
+    // Determine if the user is player1 or player2
+
+    if ($user_id == $player1) {
+        $user_is_player1 = true;
+    }
+
+
 ?>
 
 <!DOCTYPE html>
@@ -49,44 +94,17 @@
 
     <h1>Get Ready for Your High Velocity Gaming Experience</h1>
     
-    <!-- Display Game Lobby Info 
-         Also, determine which curent session player is player1 and 
-         which is player2 -->
+    
+
+    <!-- Only clickable for the host. Starts the game. -->
     <?php 
-        $gameID = $_SESSION['game_id'];
-        $user_id = $_SESSION['user_id'];
-        $username = $_SESSION['username'];
-        $private = getIsPrivate($gameID); //from dbGetters.php
-        $players = getPlayers($gameID); //from dbGetters.php
-        $player1 = $players[0]; //player1 according to db (game creator)
-        $player2 = $players[1]; //player2 according to db
-                                //null if only 1 player in game
-        
-
-        echo '<p>game_id: ';
-        echo "</p>";
-        echo $gameID;
-        echo '<p>user_id: ';
-        echo "</p>";
-        echo $user_id;
-        echo '<p>username: ';
-        echo "</p>";
-        echo $username; 
-        echo "<p>Game is private: "; 
-        echo $private;
-        echo "</p>";
-        echo "<p>Player1 ID: "; 
-        echo $player1;
-        echo "</p>";
-        echo "<p>Player2 ID: "; 
-        echo $player2;
-        echo "</p>";
+        if ($user_is_player1) {
+            echo "<button>Start</button>";
+        }
     ?>
+    
 
-    <!-- Only clickable for the host -->
-    <button>Start</button>
-
-    <!-- People can join this -->
+    <!-- Allows Player1 to End the Game. Allows Player2 to leave -->
     <button>Leave</button>
     
 </body>
